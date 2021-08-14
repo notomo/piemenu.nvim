82dd4a65c278ade7d6c98fd65c72e03a78aea17c
notomo <notomo.motono@gmail.com>
2021-08-14 22:55:13 +0900

    Refactor angle

--- lua/piemenu/core/circle_range.lua
+++ lua/piemenu/core/circle_range.lua
@@ -1,13 +1,16 @@
 local M = {}
 
+local angle_0_to_360 = function(raw_angle)
+  vim.validate({raw_angle = {raw_angle, "number"}})
+  return (raw_angle + 360) % 360
+end
+
 local CircleRange = {}
 CircleRange.__index = CircleRange
 M.CircleRange = CircleRange
 
 function CircleRange.new(start_angle, end_angle)
-  start_angle = (start_angle + 360) % 360
-  end_angle = (end_angle + 360) % 360
-  local tbl = {_s = start_angle, _e = end_angle}
+  local tbl = {_s = angle_0_to_360(start_angle), _e = angle_0_to_360(end_angle)}
   return setmetatable(tbl, CircleRange)
 end
 
@@ -15,16 +18,16 @@ function CircleRange.include(self, p1, p2)
   local x = p2[2] - p1[2]
   local y = p2[1] - p1[1]
   local rad = math.atan(y / x)
-  local angle = rad * 180 / math.pi
+  local raw_angle = rad * 180 / math.pi
   if x < 0 then
-    angle = angle + 180
+    raw_angle = raw_angle + 180
   end
-  angle = (angle + 360) % 360
 
-  if self._e < self._s then
-    return (0 <= angle and angle <= self._e) or (self._s <= angle and angle <= 360)
+  local angle = angle_0_to_360(raw_angle)
+  if self._s <= self._e then
+    return self._s <= angle and angle <= self._e
   end
-  return self._s <= angle and angle <= self._e
+  return (0 <= angle and angle <= self._e) or (self._s <= angle and angle <= 360)
 end
 
 return M
