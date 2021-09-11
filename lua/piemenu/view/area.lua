8d2d20b48e99e621f84d43a28ffba6802db66af2
notomo <notomo.motono@gmail.com>
2021-09-11 20:13:31 +0900

    Fix calc overflow

--- lua/piemenu/view/area.lua
+++ lua/piemenu/view/area.lua
@@ -18,26 +18,19 @@ function TileArea.new()
   return setmetatable(tbl, TileArea)
 end
 
-function TileArea.include(self, row, col, width, height)
-  if row <= self._min_row or self._max_row <= row + height then
-    return false
-  end
-  return self._min_col <= col and col + width < self._max_col
-end
-
 function TileArea.calculate_overflow(self, radius, origin_pos, width, height)
   local raw_angle_ranges = {}
   local directions = {
-    {90, self._max_row - origin_pos[1], height / 2},
-    {180, self._min_col - origin_pos[2], width / 2 + radius},
-    {270, self._min_row - origin_pos[1], height / 2},
-    {360, self._max_col - origin_pos[2], width / 2 + radius}, -- +radius for row height and col width ratio
+    {0, self._max_col - origin_pos[2], radius * 2 + width / 2, 15}, -- radius * 2 for row height and col width ratio
+    {90, self._max_row - origin_pos[1], radius + height / 2, 0},
+    {180, self._min_col - origin_pos[2], radius * 2 + width / 2, 15},
+    {270, self._min_row - origin_pos[1], radius + height / 2, 0},
   }
   for _, e in ipairs(directions) do
-    local base_angle, distance, extend_radius = unpack(e)
-    local rad = math.acos(math.abs(distance) / (radius + extend_radius))
+    local base_angle, distance, r, ext_angle = unpack(e)
+    local rad = math.acos(math.abs(distance) / r)
     if rad == rad then -- not nan
-      local angle = Angle.from_radian(rad)
+      local angle = Angle.from_radian(rad) + ext_angle
       table.insert(raw_angle_ranges, AngleRange.new_0_to_360(base_angle - angle, base_angle))
       table.insert(raw_angle_ranges, AngleRange.new_0_to_360(base_angle, base_angle + angle))
     end
