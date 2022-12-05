cacaa08af8d0456afb3ff6cdf2fedf1fb63fcb65
notomo <notomo.motono@gmail.com>
2022-12-05 09:14:46 +0900

    Gen doc to add language to code block

--- spec/lua/piemenu/doc.lua
+++ spec/lua/piemenu/doc.lua
@@ -87,7 +87,7 @@ If the circle is clipped by editor area, spacers are omitted.
     {
       name = "EXAMPLES",
       body = function()
-        return util.help_code_block_from_file(example_path)
+        return util.help_code_block_from_file(example_path, { language = "lua" })
       end,
     },
   },
