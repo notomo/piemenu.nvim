722c9cfcd391b9d0abc9a91e1b87925bddd1c9fa
notomo <notomo.motono@gmail.com>
2023-07-31 09:25:32 +0900

    Use vim.iter instead of continue

--- lua/piemenu/view/area.lua
+++ lua/piemenu/view/area.lua
@@ -27,10 +27,10 @@ function TileArea.calculate_overflow(self, radius, origin_pos, width, height)
     { 180, self._min_col - origin_pos[2], radius * 2 + width / 2, 15 },
     { 270, self._min_row - origin_pos[1], radius + height / 2, 5 }, -- HACK: 5?
   }
-  for _, e in ipairs(directions) do
+  vim.iter(directions):each(function(e)
     local base_angle, distance, r, ext_angle = unpack(e)
     if not self._angle_range:contain(base_angle) then
-      goto continue
+      return
     end
 
     local rad = math.acos(math.abs(distance) / r)
@@ -39,9 +39,7 @@ function TileArea.calculate_overflow(self, radius, origin_pos, width, height)
       table.insert(raw_angle_ranges, AngleRange.new_0_to_360(base_angle - angle, base_angle))
       table.insert(raw_angle_ranges, AngleRange.new_0_to_360(base_angle, base_angle + angle))
     end
-
-    ::continue::
-  end
+  end)
   return AngleRanges.new(raw_angle_ranges)
 end
 
