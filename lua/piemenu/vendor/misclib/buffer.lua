75b7866fc52757a2fc8583278ac3ef23bf9e549a
notomo <notomo.motono@gmail.com>
2024-06-28 19:13:08 +0900

    Update vendor

--- lua/piemenu/vendor/misclib/buffer.lua
+++ lua/piemenu/vendor/misclib/buffer.lua
@@ -1,17 +1,20 @@
 local M = {}
 
-function M.delete_by_name(name)
-  local bufnr = vim.fn.bufnr(("^%s$"):format(name))
-  if bufnr == -1 then
+function M.delete_by_name(path)
+  local bufnr = M.find(path)
+  if not bufnr then
     return
   end
   vim.api.nvim_buf_delete(bufnr, { force = true })
 end
 
-function M.set_lines_as_modifiable(bufnr, s, e, strict, lines)
-  vim.bo[bufnr].modifiable = true
-  vim.api.nvim_buf_set_lines(bufnr, s, e, strict, lines)
-  vim.bo[bufnr].modifiable = false
+function M.find(path)
+  local pattern = "^" .. vim.fn.escape(path, "[]{}") .. "$"
+  local bufnr = vim.fn.bufnr(pattern)
+  if bufnr == -1 then
+    return nil
+  end
+  return bufnr
 end
 
 return M
